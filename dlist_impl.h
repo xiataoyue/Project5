606398db20b5baf8cd5976f7f7864363c14d94c7
xiataoyue <xiataoyue123456@outlook.com>
2020-07-29 18:54:43 +0800

    finish project5

---
+++ dlist_impl.h
@@ -0,0 +1,140 @@
+#ifndef PROJECT_5_RELATED_FILES_DLIST_IMPL_H
+#define PROJECT_5_RELATED_FILES_DLIST_IMPL_H
+
+#include <iostream>
+#include "dlist.h"
+
+template <class T>
+bool Dlist<T>::isEmpty() const {
+    return first == NULL;
+}
+
+template <class T>
+void Dlist<T>::insertFront(T *op) {
+    if(isEmpty()){
+        node *temp = new node;
+        temp->op = op;
+        temp->next = NULL;
+        temp->prev = NULL;
+        first = temp;
+        last = temp;
+    }
+    else{
+        node *temp = new node;
+        temp->op = op;
+        temp->prev = NULL;
+        temp->next = first;
+        first->prev = temp;
+        first = temp;
+    }
+}
+
+template <class T>
+void Dlist<T>::insertBack(T *op) {
+    if(last == NULL){
+        node *temp = new node;
+        temp->op = op;
+        temp->prev = NULL;
+        temp->next = NULL;
+        first = temp;
+        last = temp;
+    }
+    else{
+        node *temp = new node;
+        temp->op = op;
+        temp->prev = last;
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+template <class T>
+T *Dlist<T>::removeFront() {
+    if(isEmpty()) throw emptyList{};
+    node *rm = first;
+    T *temp = first->op;
+    if(first == last) {
+        last = NULL;
+        first = NULL;
+    }
+    else{
+        first = first->next;
+        first->prev = NULL;
+    }
+    delete rm;
+    return temp;
+}
+
+template <class T>
+T *Dlist<T>::removeBack() {
+    if(isEmpty()) throw emptyList{};
+    node *rm = last;
+    T *temp = last->op;
+    if(first == last) {
+        first = NULL;
+        last = NULL;
+    }
+    else {
+        last = last->prev;
+        last->next = NULL;
+    }
+    delete rm;
+    return temp;
+}
+
+template <class T>
+T *Dlist<T>::remove(bool (*cmp)(const T*, const T*), T* ref){
+    node *temp = first;
+    while(temp != NULL){
+        if(cmp(temp->op, ref)){
+            T *tmp = temp->op;
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            delete temp;
+            temp = NULL;
+            return tmp;
+        }
+        temp = temp->next;
+    }
+    return nullptr;
+}
+
+template <class T>
+Dlist<T>::Dlist(): first(NULL), last(NULL) {}
+
+template <class T>
+Dlist<T>::Dlist(const Dlist<T> &l): first(NULL), last(NULL) {
+    copyAll(l);
+}
+
+template <class T>
+Dlist<T> &Dlist<T>::operator=(const Dlist<T> &l) {
+    copyAll(l);
+    return *this;
+}
+
+template <class T>
+Dlist<T>::~Dlist(){
+    removeAll();
+}
+template <class T>
+void Dlist<T>::removeAll() {
+    while(first != NULL && last != NULL && first != last){
+        removeFront();
+        removeBack();
+    }
+    if(first == last && first != NULL) removeFront();
+}
+
+template <class T>
+void Dlist<T>::copyAll(const Dlist<T> &l) {
+    if(l.isEmpty()) return;
+    if(!isEmpty()) removeAll();
+    node *temp = l.first;
+    while(temp != NULL){
+        insertBack(temp->op);
+        temp = temp->next;
+    }
+}
+#endif //PROJECT_5_RELATED_FILES_DLIST_IMPL_H
---
+++ rpn.cpp
@@ -0,0 +1,173 @@
+#include "dlist.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+template <class T>
+class Stack {
+    Dlist<T> dlist;
+
+public:
+    bool empty();
+    T *top();
+    void push(T *op);
+    void pop();
+};
+
+template <class T>
+bool Stack<T>::empty() {
+    return dlist.isEmpty();
+}
+
+template <class T>
+T *Stack<T>::top() {
+    T *op = dlist.removeFront();
+    dlist.insertFront(op);
+    return op;
+}
+
+template <class T>
+void Stack<T>::push(T *op) {
+    dlist.insertFront(op);
+}
+
+template <class T>
+void Stack<T>::pop() {
+    dlist.removeFront();
+}
+
+int main() {
+    istringstream Istream;
+    string input[1000];
+    int count = 0;
+    string str;
+    getline(cin, str);
+    Istream.str(str);
+
+    while(Istream){
+        Istream >> input[count];
+        count ++;
+    }
+    if(input[count - 1] == "\0") count --;
+
+    Stack<char> stack;
+    string output;
+
+    try {
+        for(int i = 0; i < count; i++) {
+            if(input[i] == "+" || input[i] == "-" || input[i] == "*" || input[i] == "/"){
+                while((!stack.empty()) && ((*stack.top() == '*' || *stack.top() == '/') || (input[i] == "+" || input[i] == "-")) && (*stack.top() != '(')){
+                    output += *stack.top();
+                    output += " ";
+                    stack.pop();
+                }
+                stack.push(const_cast<char *>(input[i].c_str()));
+            }
+            else if(input[i] == "(") {
+                stack.push(const_cast<char *>(input[i].c_str()));
+            }
+            else if(input[i] == ")") {
+                if(stack.empty()){
+                    string err = "ERROR: Parenthesis mismatch";
+                    throw err;
+                }
+                while(*stack.top() != '(') {
+                    output += stack.top();
+                    output += " ";
+                    stack.pop();
+                    if(stack.empty()) {
+                        string error = "ERROR: Parenthesis mismatch";
+                        throw error;
+                    }
+                }
+                stack.pop();
+            }
+            else{
+                output += input[i];
+                output += " ";
+            }
+        }
+        while(!stack.empty()) {
+            if(*stack.top() == '(' || *stack.top() == ')') {
+                string mis = "ERROR: Parenthesis mismatch";
+                throw mis;
+            }
+            output += *stack.top();
+            output += " ";
+            stack.pop();
+        }
+        cout << output << endl;
+
+        Istream.clear();
+        Istream.str(output);
+        Stack<long long int> cal;
+
+        while(Istream) {
+            string ch;
+            Istream >> ch;
+            if(ch == "\0") break;
+            if(ch == "+" || ch == "-" || ch == "*" || ch == "/") {
+                if(ch.empty()) continue;
+                if(cal.empty()) {
+                    string neo = "ERROR: Not enough operands";
+                    throw neo;
+                }
+                long long int *back = cal.top();
+                cal.pop();
+                if(cal.empty()){
+                    string neo = "ERROR: Not enough operands";
+                    delete back;
+                    throw neo;
+                }
+                long long int *front = cal.top();
+                cal.pop();
+                if(ch == "+") {
+                    auto *temp_result = new long long int(*front + *back);
+                    cal.push(temp_result);
+                }
+                else if(ch == "-") {
+                    auto *temp_result = new long long int(*front - *back);
+                    cal.push(temp_result);
+                }
+                else if(ch == "*") {
+                    auto *temp_result = new long long int(*front * *back);
+                    cal.push(temp_result);
+                }
+                else if(ch == "/") {
+                    if(*back == 0) {
+                        string dbz = "ERROR: Divide by zero";
+                        delete front;
+                        delete back;
+                        throw dbz;
+                    }
+                    auto *temp_result = new long long int(*front / *back);
+                    cal.push(temp_result);
+                }
+                delete front;
+                delete back;
+            }
+            else{
+                long int x = atoi(ch.c_str());
+                auto *val = new long long int(x);
+                cal.push(val);
+            }
+        }
+        long long int *result = cal.top();
+        cal.pop();
+        if(!cal.empty()) {
+            string tmo = "ERROR: Too many operands";
+            delete result;
+            throw tmo;
+        }
+        cout << *result << endl;
+        delete result;
+    }
+    catch (string exception) {
+        cout << exception << endl;
+    }
+
+    return 0;
+}
